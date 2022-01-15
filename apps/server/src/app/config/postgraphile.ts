fd1db53a31bff451b415e9a13f7436b64bc6bb42
Akash Singh <meteorain.dev@gmail.com>
2022-01-15 23:52:15 +0200

    Differentiate developments options better

--- apps/server/src/app/config/postgraphile.ts
+++ apps/server/src/app/config/postgraphile.ts
@@ -16,35 +16,40 @@ const {
   DEPLOYMENT,
 } = process.env
 
-export default postgraphile(
-  {
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    host: POSTGRES_HOST,
-    port: POSTGRES_PORT,
-    ...(DEPLOYMENT === true ? { ssl: { rejectUnauthorized: false } } : {}),
-  },
-  {
-    pluginHook,
-    subscriptions: true,
-    appendPlugins: [MySubscriptionPlugin, ConnectionFilterPlugin],
-    simpleSubscriptions: true,
-    watchPg: true,
-    graphiql: true,
-    enhanceGraphiql: true,
-    jwtPgTypeIdentifier: 'public.jwt_token',
-    jwtSecret: 'bf',
-    ...(NODE_ENV === 'development'
-      ? { exportGqlSchemaPath: POSTGRAHILE_EXPORT_GQL_SCHEMA_PATH }
-      : {}),
-    websocketMiddlewares: [
-      // Add whatever middlewares you need here, note that they should only
-      // manipulate properties on req/res, they must not sent response data. e.g.:
-      //
-      //   require('express-session')(),
-      //   require('passport').initialize(),
-      //   require('passport').session(),
-    ],
-  }
-)
+const developmentOptions: Parameters<typeof postgraphile>[1] = NODE_ENV ===
+  'development' && {
+  watchPg: true,
+  exportGqlSchemaPath: POSTGRAHILE_EXPORT_GQL_SCHEMA_PATH,
+}
+
+// https://www.graphile.org/postgraphile/usage-library/#api-postgraphilepgconfig-schemaname-options
+export default () =>
+  postgraphile(
+    {
+      database: POSTGRES_DB,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      host: POSTGRES_HOST,
+      port: POSTGRES_PORT,
+      ...(DEPLOYMENT === true && { ssl: { rejectUnauthorized: false } }),
+    },
+    {
+      pluginHook,
+      subscriptions: true,
+      appendPlugins: [MySubscriptionPlugin, ConnectionFilterPlugin],
+      simpleSubscriptions: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+      jwtPgTypeIdentifier: 'public.jwt_token',
+      jwtSecret: 'bf',
+      ...developmentOptions,
+      websocketMiddlewares: [
+        // Add whatever middlewares you need here, note that they should only
+        // manipulate properties on req/res, they must not sent response data. e.g.:
+        //
+        //   require('express-session')(),
+        //   require('passport').initialize(),
+        //   require('passport').session(),
+      ],
+    }
+  )
