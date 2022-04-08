b4f8581eb69e3c55d5831e1cc9e59df6a1600ad7
Akash Singh <meteorain.dev@gmail.com>
2022-04-08 14:02:23 +0300

    Update script

--- shell/entrypoint.sh
+++ shell/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-cd migration
-yarn knex migrate:rollback
-yarn knex migrate:latest
-cd ../server
+yarn --cwd migration knex migrate:rollback
+yarn --cwd migration knex migrate:latest
+cd server
 node main.js
