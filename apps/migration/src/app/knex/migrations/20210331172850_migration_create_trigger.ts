c6f9c861a89e4a35ed082fcc6da88b4a3b930b13
Akash Singh <meteorain.dev@gmail.com>
2023-07-15 12:17:12 +0300

    (chore) rename

--- apps/migration/src/app/knex/migrations/20210331172850_migration_create_trigger.ts
+++ apps/migration/src/app/knex/migrations/20210331172850_migration_create_trigger.ts
@@ -1,84 +1,84 @@
 import { Knex } from 'knex'
 
-const FUNCTION_GRAPHQL_SUBSCRIPTION = `create function graphql_subscription() returns trigger as $$
-declare
-  v_process_new bool = (TG_OP = 'INSERT' OR TG_OP = 'UPDATE');
-  v_process_old bool = (TG_OP = 'UPDATE' OR TG_OP = 'DELETE');
-  v_event text = TG_ARGV[0];
-  v_topic_template text = TG_ARGV[1];
-  v_attribute text = TG_ARGV[2];
+const FUNCTION_GRAPHQL_SUBSCRIPTION = `CREATE FUNCTION graphql_subscription() returns trigger AS $$
+DECLARE
+  v_process_new BOOL = (TG_OP = 'INSERT' OR TG_OP = 'UPDATE');
+  v_process_old BOOL = (TG_OP = 'UPDATE' OR TG_OP = 'DELETE');
+  v_event TEXT = TG_ARGV[0];
+  v_topic_template TEXT = TG_ARGV[1];
+  v_attribute TEXT = TG_ARGV[2];
   v_record record;
-  v_sub text;
-  v_topic text;
+  v_sub TEXT;
+  v_topic TEXT;
   v_i int = 0;
-  v_last_topic text;
-begin
+  v_last_topic TEXT;
+BEGIN
   -- On UPDATE sometimes topic may be changed for NEW record,
   -- so we need notify to both topics NEW and OLD.
-  for v_i in 0..1 loop
-    if (v_i = 0) and v_process_new is true then
-      v_record = new;
-    elsif (v_i = 1) and v_process_old is true then
-      v_record = old;
-    else
+  FOR v_i IN 0..1 LOOP
+    IF (v_i = 0) AND v_process_new IS true THEN
+      v_record = NEW;
+    ELSIF (v_i = 1) AND v_process_old IS true THEN
+      v_record = OLD;
+    ELSE
       continue;
-    end if;
-     if v_attribute is not null then
-      execute 'select $1.' || quote_ident(v_attribute)
-        using v_record
-        into v_sub;
-    end if;
-    if v_sub is not null then
+    END IF;
+     IF v_attribute IS NOT NULL THEN
+      EXECUTE 'SELECT $1.' || quote_ident(v_attribute)
+        USING v_record
+        INTO v_sub;
+    END IF;
+    IF v_sub IS NOT NULL THEN
       v_topic = replace(v_topic_template, '$1', v_sub);
-    else
+    ELSE
       v_topic = v_topic_template;
-    end if;
-    if v_topic is distinct from v_last_topic then
+    END IF;
+    IF v_topic IS DISTINCT FROM v_last_topic THEN
       -- This if statement prevents us from triggering the same notification twice
       v_last_topic = v_topic;
-      perform pg_notify(v_topic, json_build_object(
+      PERFORM pg_notify(v_topic, json_build_object(
         'event', v_event,
         'subject', NEW
-      )::text);
-    end if;
-  end loop;
+      )::TEXT);
+    END IF;
+  END LOOP;
   return v_record;
-end;
-$$ language plpgsql volatile set search_path from current`
+END;
+$$ LANGUAGE plpgsql VOLATILE SET search_path FROM current`
 
-const FUNCTION_SUBSCRIPTION_NEW_CHANNEL = `create function graphql_subscription_new_channel() returns trigger as $$
-declare
-  v_event text = 'newChannel';
-  v_topic_template text = TG_ARGV[0];
-begin
-  perform pg_notify('graphql:user:' || NEW.member_id || ':channel', json_build_object(
+const FUNCTION_SUBSCRIPTION_NEW_CHANNEL = `CREATE FUNCTION graphql_subscription_new_channel() returns trigger AS $$
+DECLARE
+  v_event TEXT = 'newChannel';
+  v_topic_template TEXT = TG_ARGV[0];
+BEGIN
+  PERFORM pg_notify('graphql:user:' || NEW.member_id || ':channel', json_build_object(
     'event', v_event,
     'subject', NEW
-  )::text);
+  )::TEXT);
 
   return NEW;
-end;
-$$ language plpgsql volatile set search_path from current`
-const FUNCTION_SUBSCRIPTION_NEW_MESSAGE = `create function graphql_subscription_new_message() returns trigger as $$
-declare
-  v_event text = 'newMessage';
-begin
-  perform pg_notify('graphql:channel:' || NEW.channel_id, json_build_object(
+END;
+$$ LANGUAGE plpgsql VOLATILE SET search_path FROM current`
+const FUNCTION_SUBSCRIPTION_NEW_MESSAGE = `CREATE FUNCTION graphql_subscription_new_message() returns trigger AS $$
+DECLARE
+  v_event TEXT = 'newMessage';
+BEGIN
+  PERFORM pg_notify('graphql:channel:' || NEW.channel_id, json_build_object(
     'event', v_event,
     'subject', NEW
-  )::text);
+  )::TEXT);
   return NEW;
-end;
-$$ language plpgsql volatile set search_path from current`
-const FUNCTION_CREATE_CHANNEL = `create function create_channels_for_user(
-  member_id uuid
-) returns setof channel as $$
-declare
+END;
+$$ LANGUAGE plpgsql VOLATILE SET search_path FROM current`
+const FUNCTION_CREATE_CHANNEL = `CREATE FUNCTION create_channels_for_user(
+  member_id UUID
+) returns setof channel AS $$
+DECLARE
   v_member member;
   v_channel channel;
   v_setof_channel record;
-begin
-  -- Select all users except the newly created one
+BEGIN
+  -- select all users except the newly created one
   FOR v_member IN (SELECT * FROM member WHERE id != member_id) LOOP
     -- Create new channel for member
     INSERT INTO channel DEFAULT VALUES RETURNING * INTO v_channel;
@@ -93,59 +93,59 @@ begin
   END LOOP;
 
   RETURN;
-end;
-$$ language plpgsql volatile;`
+END;
+$$ LANGUAGE plpgsql VOLATILE;`
 
-const FUNCTION_REGISTER_USER = `create function register_member(
-  first_name text,
-  last_name text,
-  email text,
-  password text
-) returns member as $$
-declare
-  member member;
-begin
-  insert into member (first_name, last_name) values
+const FUNCTION_REGISTER_USER = `CREATE FUNCTION register_member(
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  password TEXT
+) returns member AS $$
+DECLARE
+  v_member member;
+BEGIN
+  INSERT INTO member (first_name, last_name) VALUES
     (first_name, last_name)
-    returning * into member;
+    RETURNING * INTO v_member;
 
-  insert into member_account (member_id, email, password_hash) values
-    (member.id, email, crypt(password, gen_salt('bf')));
+  INSERT INTO member_account (member_id, email, password_hash) VALUES
+    (v_member.id, email, crypt(password, gen_salt('bf')));
 
-  PERFORM create_channels_for_user(member.id);
+  PERFORM create_channels_for_user(v_member.id);
 
-  return member;
-end;
-$$ language plpgsql strict security definer;`
+  return v_member;
+END;
+$$ LANGUAGE plpgsql STRICT SECURITY DEFINER;`
 
-const FUNCTION_AUTHENTICATE = `create function authenticate(
-  email text,
-  password text
-) returns jwt_token as $$
-declare
+const FUNCTION_AUTHENTICATE = `CREATE FUNCTION authenticate(
+  email TEXT,
+  password TEXT
+) returns jwt_token AS $$
+DECLARE
   account member_account;
-begin
-  select a.* into account
-  from member_account as a
-  where a.email = $1;
+BEGIN
+  SELECT a.* INTO account
+  FROM member_account AS a
+  WHERE a.email = $1;
 
-  if account.password_hash = crypt(password, account.password_hash) then
-    return ('${process.env.ROLE}', account.member_id, extract(epoch from (now() + interval '2 days')))::jwt_token;
-  else
+  IF account.password_hash = crypt(password, account.password_hash) THEN
+    return ('${process.env.ROLE}', account.member_id, extract(epoch FROM (now() + interval '2 days')))::jwt_token;
+  ELSE
     return null;
-  end if;
-end;
-$$ language plpgsql strict security definer;`
-const FUNCTION_CURRENT_USER = `create function current_member() returns member as $$
-  select *
-  from member
-  where id = nullif(current_setting('jwt.claims.member_id', true), '')::uuid
-$$ language sql stable;`
+  END IF;
+END;
+$$ LANGUAGE plpgsql STRICT SECURITY DEFINER;`
+const FUNCTION_CURRENT_USER = `CREATE FUNCTION current_member() returns member AS $$
+  SELECT *
+  FROM member
+  WHERE id = nullif(current_setting('jwt.claims.member_id', true), '')::UUID
+$$ LANGUAGE sql stable;`
 
-const TYPE_JWT_TOKEN = `create type jwt_token as (
-  role text,
-  member_id uuid,
-  exp bigint
+const TYPE_JWT_TOKEN = `CREATE TYPE jwt_token AS (
+  role TEXT,
+  member_id UUID,
+  exp BIGINT
 );`
 
 const TRIGGER_NEW_CHANNEL = `CREATE TRIGGER trigger_new_channel
